fb0daf186877fd26f48296f320bf07170c7b0758
scjs <sseyfarth@ucsd.edu>
2015-04-13 21:38:22 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='buckeye',
+    version='1.0',
+    packages=['buckeye'],
+    description='Iterators for the Buckeye Corpus',
+    url='http://github.com/scjs/buckeye/',
+    )
