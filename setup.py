81c0c5200de54d709d1844421a8d1833a089b7d7
scjs <sseyfarth@ucsd.edu>
2016-01-06 18:33:48 -0800

    update version and add nose to setup

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup
 
+import multiprocessing
+
 setup(
     name='buckeye',
-    version='1.1',
+    version='1.2',
     packages=['buckeye'],
     description='Classes and iterators for the Buckeye Corpus',
     url='http://github.com/scjs/buckeye/',
+    test_suite='nose.collector',
+    tests_require=['nose', 'mock'],
     )
